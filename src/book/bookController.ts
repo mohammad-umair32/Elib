c7698179faaa6ee3a0f26cd8dbbf7db7c018f5cf
mohammad-umair32 <mohammadumair.720855@gmail.com>
2025-07-28 19:46:28 +0530

    add book model

---
+++ src/book/bookController.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+const createBook = async (req: Request, res: Response, next: NextFunction) =>{
+    res.json({});
+}
+
+
+
+export { createBook };
