699c967178f2821fc27a8e058a05e3fc7d541bcb
mohammad-umair32 <mohammadumair.720855@gmail.com>
2025-07-28 18:57:21 +0530

    sign jwt token

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -19,11 +21,21 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
   //password=>hashed
   const hashedPassword = await bcrypt.hash(password, 10);
-  
 
-  //process
+  const newUser = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  //Token generation JWT
+  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+    algorithm:'HS256',
+  });
+
   //response
-  res.json({ message: "User Created" });
+  res.json({ accessToken: token });
 };
 
 export { createUser };
